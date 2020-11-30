538b6b3f22960eba2912632e09f7a0274b48b674
Shaik Masihullah <masihulla17@gmail.com>
2020-11-30 16:38:37 +0530

    Added Face Encodings and Segmentation

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,6 +3,12 @@ import pandas as pd
 import pickle
 from skimage import data, img_as_float
 from skimage import exposure
+import face_recognition
+import cv2
+
+eyesDetector = cv2.CascadeClassifier('./haarcascades/eyes.xml')
+mouthDetector = cv2.CascadeClassifier('./haarcascades/mouth.xml')
+noseDetector = cv2.CascadeClassifier('./haarcascades/nose.xml')
 
 # Preprocessing Functions
 def contrastStretching(image):
@@ -10,10 +16,41 @@ def contrastStretching(image):
 	img_eq = exposure.equalize_hist(image)
 	return img_eq
 
-def faceEncoding(image):
-	# Implement face encodings here
-	encoding = None
+def faceEncoding(image, model="large"):
+	# Implement face encodings
+	try:
+		image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+		encoding = face_recognition.face_encodings(image, model=model)[0]
+	except IndexError:
+		encoding = np.zeros((128,))
 	return encoding
+
+def findFaces(image, model="cnn"):
+	faces = face_recognition.face_locations(image, model=model)
+	croppedFaceImages = []
+	for face in faces:
+		top, right, bottom, left = face
+		croppedFace = image[top:bottom, left:right]
+		croppedFaceImages.append(croppedFace)
+	return croppedFaceImages
+
+def detectCascade(face, detect):
+	try:
+		if detect == "mouth":
+			size = (25, 15)
+			x, y, w, h = mouthDetector.detectMultiScale(face)[0]
+		elif detect == "eyes":
+			size = (35, 16)
+			x, y, w, h = eyesDetector.detectMultiScale(face)[0]
+		elif detect == "nose":
+			size = (25, 15)
+			x, y, w, h = noseDetector.detectMultiScale(face)[0]
+		else:
+			return None
+	except IndexError:
+		return np.zeros(size[::-1])
+	cropped = cv2.resize(face[y:y+h, x:x+w], size)
+	return cropped
 
 def generateIntensityImages(image):
 	# Implement generation of images with different intensites
@@ -24,38 +61,67 @@ def pseudoLabelledData():
 	# Loading data which has been labelled using pseudo labelling technique
 	pseudoImages = []
 	pseudoLabels = []
-	return (pseudoImages, pesudoLabels)
+	return (pseudoImages, pseudoLabels)
+
+def applyPreProcessing(image, label):
+	global generatedImages, generatedLabels, generatedEncodings, eyes, noses, mouths
+	generatedImages.append(image)
+	generatedLabels.append(label)
+	generatedEncodings.append(faceEncoding(image))
+	eyes.append(detectCascade(image, detect="eyes"))
+	noses.append(detectCascade(image, detect="nose"))
+	mouths.append(detectCascade(image, detect="mouth"))
+
+def processImage(image, label):
+	for face in findFaces(image):
+		grayFace = cv2.cvtColor(face, cv2.COLOR_RGB2GRAY)
+		grayFace = contrastStretching(grayFace)
+		applyPreProcessing(grayFace, label)
+
+		for generated in generateIntensityImages(grayFace):
+			applyPreProcessing(generated, label)
 
 if __name__ == "__main__":
 	# Loading the dataset
-	dataset = pd.read_csv("fer2013.csv")
+	dataset = pd.read_csv("./data/fer2013.csv")
 
 	for category in dataset['Usage'].unique():
 		categoryData = dataset[dataset['Usage'] == category]
 		samples = categoryData['pixels'].values
 		labels = categoryData['emotion'].values
-		encodings = list()
+
+		eyes = list()
+		noses = list()
+		mouths = list()
 		generatedImages = []
 		generatedLabels = []
 		generatedEncodings = []
 
+		print("Total {} {} images".format(len(samples), category))
 		for i in range(len(samples)):
 			try:
-				image = np.fromstring(samples[i], dtype=int, sep=" ").reshape((48, 48))
-				image = contrastStretching(image)
-				encodings[i] = faceEncoding(image)
-				generatedImages += generateIntensityImages(image)
-				generatedLabels += [labels[i]] * len(generatedImages)
-				for j in range(len(generatedImages)):
-					generatedEncodings[j] = faceEncoding(generatedImages[j])
+				image = np.fromstring(samples[i], dtype=int, sep=" ").reshape((48, 48)).astype(np.uint8)
+				image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+				processImage(image, labels[i])
+				if (i+1) % 1000 == 0:
+					print("Done %d images."%i)
 			except Exception as e:
 				print("Error : " + str(e))
-		
-		samples += generatedImages
-		labels += generatedLabels
-		encodings += generatedEncodings
 
-		data = {'X' : samples, 'y' : labels, 'encodings' : encodings}
-		with open(category + "_data.pickle", 'wb') as f:
+		if category == "Training":
+			samples, labels = pseudoLabelledData()
+			print("Total %d pseudo labelled images"%len(samples))
+			for i in range(len(samples)):
+				try:
+					processImage(samples[i], labels[i])
+					if (i+1) % 1000 == 0:
+						print("Done %d pseudo labelled images."%i)
+				except Exception as e:
+					print("Error : " + str(e))
+
+		print({'X' : len(generatedImages), 'y' : len(generatedLabels), 'encodings' : len(generatedEncodings), 'eyes' : len(eyes), 'noses' : len(noses), 'mouths' : len(mouths)})
+
+		data = {'X' : generatedImages, 'y' : generatedLabels, 'encodings' : generatedEncodings, 'eyes' : eyes, 'noses' : noses, 'mouths' : mouths}
+		with open("./data/" + category + "_data.pickle", 'wb') as f:
 			pickle.dump(data, f)
 			f.close()
--- test.py
+++ test.py
@@ -1,15 +1,29 @@
-from preprocess_data import contrastStretching, faceEncoding, generateIntensityImages, pseudoLabelledData
+from preprocess_data import contrastStretching, faceEncoding, generateIntensityImages, pseudoLabelledData, findFaces, detectCascade
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import face_recognition
 
 image = cv2.imread('test_image.jpg')
 #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Replace function with the one, you are working on
-processedImage = contrastStretching(image)
+# processedImage = contrastStretching(image)
+
+processedImage = findFaces(image)
+gray = cv2.cvtColor(processedImage[0], cv2.COLOR_RGB2GRAY)
+
+print(faceEncoding(gray, model="large"))
+
+detected = detectCascade(gray, "mouth")
+
+plt.imshow(gray)
+plt.show()
+
+plt.imshow(detected)
+plt.show()
 
 # Left image is original image and right image is the processed image
-plt.imshow(np.concatenate((image, processedImage), axis=1), cmap=cm.gray, vmin=0, vmax=255)
-plt.show()
+# plt.imshow(np.concatenate((image, processedImage), axis=1), cmap=cm.gray, vmin=0, vmax=255)
+# plt.show()
